662dc8de88e9e2391db9c45a376667ead2a10228
ENRIQUE ARCE ALEJANDRO <a2019112108@estudiantes.upsa.edu.bo>
2020-08-18 21:37:56 -0400

    Solución corregida sin decimales.

---
+++ WebApplication1/WebApplication1/Controllers/AppController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Helpers;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        [HttpGet]
+        public string App(int numero)
+        {
+            string cadena = Conversores.NumeroALetras(numero);
+            return cadena;
+        }
+        [HttpPost]
+        public string App1([FromHeader]int numero)
+        {
+            string cadena = Conversores.NumeroALetras(numero);
+            return cadena;
+        }
+    }
+}
